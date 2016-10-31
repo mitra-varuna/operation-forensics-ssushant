23a47d79b38d62c1a42c79713b8abf2963fafbe7
Sushant Srivastava <sushant.srivastav@gmail.com>
2016-10-31 21:15:50 +0530

    Added code for getting wikipedia URL

--- app/runners.py
+++ app/runners.py
@@ -14,27 +14,12 @@ def feed_index():
         for entry in d.entries:
             parse_single_url(entry.link, entry.description)
 
-def index():
-    response = []
-    for url in URLS:
-        try:
-            result = urlfetch.fetch(url)
-            if result.status_code == 200:
-                content = hindu_strategy(url, result)
-                #response.append(get_sentiment(content))
-            else:
-                return {}
-        except urlfetch.Error:
-            logging.exception('Caught exception fetching url')
-    return response
-    # return hindu_strategy(None, None)
 
 def parse_single_url(url, summary):
     try:
         result = urlfetch.fetch(url)
         if result.status_code == 200:
             content = hindu_strategy(url, result, summary)
-            #response.append(get_sentiment(content))
         else:
             return {}
     except urlfetch.Error:
@@ -50,6 +35,41 @@ def hindu_strategy(url, result, summary):
     content = ''.join((p.text for p in bs.findAll('p',{'class':'body'})))
     #summary = bs.findAll('h1', {'class':'detail-title'})[0].text
     analyse = sentiments.get_sentiment(content.replace('\n',''))
-    entities = [Entity(photo_url=None, wikipedia_url=r['url'], name=r['name'],mention_type=r['mention_type'],salience=r['salience']) for r in analyse['entities']]
+    wikipedia_url = r['url']
+    entities = [Entity(photo_url=get_wikipedia_image(wikipedia_url), wikipedia_url=r['url'], name=r['name'],mention_type=r['mention_type'],salience=r['salience']) for r in analyse['entities']]
     ops = OpSummary(url=url, summary=summary, polarity=analyse['polarity'], entities=entities,magnitude=analyse['magnitude'])
     ops.put()
+
+    
+def get_wikipedia_image(wikipedia_url):
+    title = get_title(wikipedia_url)
+    if title:
+        images = get_image(title)
+        if images:
+            return images
+        else:
+            return None
+    else:
+        return None
+
+
+def get_title(wikipedia_url):
+    """Get the title given a wikipedia URL"""
+    if wikipedia_url:
+        (_, _, title) = wikipedia_url.partition("/wiki/")
+        return title
+    else:
+        return None
+
+def get_image(title):
+    """Gets the list of images associated with the wikipedia title"""
+    url = "https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles={0}".format(title)
+    try:
+        json_info = urlfetch.fetch(url)
+        info = json.loads(json_info)
+        for  i in info['query']['pages'].values():
+            if 'thumbnail' in i:
+                image_url = i['thumbnail']['original']
+                return image_url
+    except Exception:
+        return None
