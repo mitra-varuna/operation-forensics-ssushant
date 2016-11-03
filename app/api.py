a7e1102cc8193a43391019780f175586b8cdffd6
Sushant Srivastava <sushant.srivastav@gmail.com>
2016-11-03 16:35:26 +0530

    Added date property

--- app/api.py
+++ app/api.py
@@ -1,6 +1,8 @@
+import datetime
+
 from models import OpSummary
 
 
 def all_articles():
-    return [f.to_dict(exclude=['date']) for f in OpSummary.query().fetch(
+    return [f.to_dict(exclude=['date']) for f in OpSummary.query(OpSummary.when == datetime.date.today()).fetch(
         projection=[OpSummary.summary, OpSummary.url, OpSummary.polarity, OpSummary.magnitude])]
